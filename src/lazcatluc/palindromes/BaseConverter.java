a06a422ae1a29e30712e636be6ae868fe50319f6
lazcatluc <lazcatluc@gmail.com>
2015-02-26 14:25:35 +0200

    changed base converter to use builder pattern

--- src/lazcatluc/palindromes/BaseConverter.java
+++ src/lazcatluc/palindromes/BaseConverter.java
@@ -3,19 +3,29 @@ package lazcatluc.palindromes;
 import java.math.BigInteger;
 
 public class BaseConverter {
-	private final Number numberToConvert;
-	private final int fromBase;
-	private final int toBase;
+
+	private Number numberToConvert = 0;
+	private int fromBase = 10;
+	private int toBase = 2;
 	
-	public BaseConverter(Number numberToConvert, int fromBase, int toBase) {
-		super();
-		this.numberToConvert = numberToConvert;
+	public BaseConverter fromBase(int fromBase) {
 		this.fromBase = fromBase;
+		return this;
+	}
+	
+	public BaseConverter toBase(int toBase) {
 		this.toBase = toBase;
+		return this;
+	}
+	
+	public BaseConverter forNumber(Number numberToConvert) {
+		this.numberToConvert = numberToConvert;
+		return this;
 	}
 	
 	public String convert() {
 		return new BigInteger(numberToConvert.toString(), fromBase)
 			.toString(toBase);
+
 	}
 }
--- test/lazcatluc/palindromes/BaseConverterTest.java
+++ test/lazcatluc/palindromes/BaseConverterTest.java
@@ -8,11 +8,13 @@ public class BaseConverterTest {
 
 	@Test
 	public void converts10To1010Base2() {
-		assertEquals("1010", new BaseConverter(10, 10, 2).convert());
+		assertEquals("1010", new BaseConverter().forNumber(10)
+					.fromBase(10).toBase(2).convert());
 	}
 	
 	@Test
 	public void converts1010To10Base10() {
-		assertEquals("10", new BaseConverter(1010, 2, 10).convert());
+		assertEquals("10", new BaseConverter().forNumber(1010)
+					.fromBase(2).toBase(10).convert());
 	}
 }
